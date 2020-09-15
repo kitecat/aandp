e0af7fb6330a2249aa40facb97bfc34ebd091120
aytal <kitecat.kitekat@gmail.com>
2020-09-15 10:23:08 +0900

    Докончил задание 5.2

--- src/Task5_2.java
+++ src/Task5_2.java
@@ -1,50 +1,39 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Task5_2 {
     public static void main(String[] args) {
         try {
-            File f1 = new File("input.txt");
-            DataInputStream rd =
-                    new DataInputStream(new FileInputStream(f1.getAbsolutePath()));
-            List<Double> input = new ArrayList<Double>();
-            try {
-                while(rd.available() > 0) {
-                    input.add(rd.readDouble());
-                    System.out.println(input.get(input.size() - 1));
-                }
-            } catch (EOFException e) {
-                rd.close();
+            List<Double> inputArray = new ArrayList<>();
+            File inputFile = new File("input.txt");
+            BufferedReader inputReader
+                    = new BufferedReader(new FileReader(inputFile));
+            String temp;
+            while ((temp = inputReader.readLine()) != null) {
+                inputArray.add(Double.parseDouble(temp));
             }
 
-//            File f2 = new File("output.txt");
-//            if (f2.exists()) {
-//                f2.delete();
-//            }
-//            f2.createNewFile();
-//
-//            DataOutputStream wr =
-//                    new DataOutputStream(new FileOutputStream(f2.getAbsolutePath()));
-//
-//            double max = 100;
-//            wr.writeDouble(max);
-//            try {
-//                while(true) {
-//                    double number = rd.readDouble();
-//                    if (number > max) {
-//                        max = number;
-//                    }
-//                    wr.writeDouble(number);
-////                    wr.writeDouble(max);
-//                    System.out.println("Maximum " + max);
-//                }
-//            } catch (EOFException e) {
-//                wr.flush();
-//                wr.close();
-//                rd.close();
-//            }
+            double max = inputArray.get(0);
+            for (double input : inputArray) {
+                if (input > max)
+                    max = input;
+            }
+
+            File outputFile = new File("output.txt");
+            if (outputFile.exists()) {
+                outputFile.delete();
+            }
+            outputFile.createNewFile();
+            DataOutputStream outputStream
+                    = new DataOutputStream(new FileOutputStream(outputFile.getAbsolutePath()));
+            for (double b : inputArray) {
+                if (b > 0)
+                    outputStream.writeUTF(String.valueOf(b));
+            }
+            outputStream.writeUTF(String.valueOf(max));
+            outputStream.flush();
+            outputStream.close();
         } catch (IOException e) {
             System.out.println("End of file");
         }
