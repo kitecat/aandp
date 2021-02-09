deb75ed8d8c887417ce8f8fd580b2255e8c6137e
aytal <kitecat.kitekat@gmail.com>
2021-02-09 16:53:41 +0900

    Сделал лабораторную 14

--- src/TooSimpleFrame.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-
-class TooSimpleFrame extends Frame {
-    public static void main(String[] args) {
-        Frame fr = new TooSimpleFrame();
-        fr.setSize(400, 150);
-        fr.setVisible(true);
-    }
-}
