405b9be2179050156e63bdc36baf879f5b2ce272
egorov_aa <egorov_aa@albank.ru>
2022-05-11 14:30:10 +0900

    Second task done

--- src/Second.java
+++ src/Second.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class Second {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int i = 1; i * i <= n; i++) {
+            System.out.print(i * i + " ");
+        }
+    }
 }
